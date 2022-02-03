93a0dad83414e5bb41f4298c13a623b85552a91e
SangHyun <duddjshfl@naver.com>
2022-02-03 14:33:41 +0900

    19장 초반 update

---
+++ 예제/19-11.js
@@ -0,0 +1,3 @@
+console.log((function () {}).hasOwnProperty('prototype'));
+
+console.log(({}).hasOwnProperty('prototype'));
---
+++ 예제/19-20.js
@@ -0,0 +1,7 @@
+
+
+function Person(name){
+    this.name = name;
+}
+
+console.log(Person.prototype);
