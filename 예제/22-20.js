6e37ed2ae6d37c624ee77549d488ac9b0efbc9fc
unknown <duddjshfl@naver.com>
2022-02-11 18:26:03 +0900

    21~23장 중반 update

---
+++ 예제/22-20.js
@@ -0,0 +1,10 @@
+function getThisBinding(){
+	console.log(arguments);
+    return this;
+}
+
+const thisArg = { a: 1 };
+
+console.log(getThisBinding.apply(thisArg, [1, 2, 3]));
+
+console.log(getThisBinding.call(thisArg, 1, 2, 3));
---
+++ 예제/22-21.js
@@ -0,0 +1,10 @@
+function convertArgsToArray(){
+	console.log(arguments);
+	
+    const arr = Array.prototype.slice.call(arguments);
+    
+    console.log(arr);
+    return arr;
+}
+
+convertArgsToArray(1, 2, 3);
