1acf9082532bcb006fda2d9bf49d2586831eea4e
SangHyun <duddjshfl@naver.com>
2022-03-08 19:07:46 +0900

    27.1 ~ 27.7장 및 예제 update

---
+++ 예제/27-16.js
@@ -0,0 +1,12 @@
+const arr = [1];
+
+arr.length = 3;
+
+console.log(Object.getOwnPropertyDescriptors(arr));
+
+/*
+{
+    '0': { value: 1, writable: true, enumerable: true, configurable: true },
+    length: { value: 3, writable: true, enumerable: false, configurable: false }
+}
+*/
---
+++ 예제/27-17.js
@@ -0,0 +1,6 @@
+const sparse = [, 2, , 4]
+
+console.log(sparse.length);
+console.log(sparse);
+
+console.log(Object.getOwnPropertyDescriptors(sparse))
