c36d97fada22a038f4e18fa9f81306584da464e0
unknown <duddjshfl@naver.com>
2022-03-11 18:04:45 +0900

    31장 정규표현식 및 예제 update

---
+++ 예제/31-27.js
@@ -0,0 +1,9 @@
+const target = 'Aa Bb 12,345 _$%&';
+
+let regExp = /[\w,]+/g;
+
+console.log(target.match(regExp))	// [ 'Aa', 'Bb', '12,345', '_' ]
+
+regExp = /[\W,]+/g;
+
+console.log(target.match(regExp))   // [ ' ', ' ', ',', ' ', '$%&' ]
