6ba6d05f3e9b0b9dc7c7442803c07e8fd2db099d
unknown <duddjshfl@naver.com>
2022-03-16 19:17:57 +0900

    33.2 ~ 35장 update

---
+++ 예제/35-01.js
@@ -0,0 +1,9 @@
+console.log(...[1, 2, 3]);	// 1 2 3
+
+console.log(...'Hello');	// H e l l o
+
+console.log(...new Map([['a', '1'], ['b', '2']]));
+
+console.log(...new Set([1, 2, 3]));	// 1 2 3
+
+console.log(...{a: 1, b: 2});
